d94c49e2564e88fd5d105142b71e74ca1da7ae5b
sdshaoda <sdshaoda@126.com>
2018-08-07 00:12:50 +0800

    add variable and some style

--- src/components/p-footer/p-footer.vue
+++ src/components/p-footer/p-footer.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="p-footer">
     <router-link tag="div" to="/record">
-      <span class="link p-icon i-caifub">流水</span>
+      <i class="p-icon i-jizhang"></i>
+      <span class="link">流水</span>
     </router-link>
     <router-link tag="div" to="/asset">
+      <i class="p-icon i-caifua"></i>
       <span class="link">资产</span>
     </router-link>
     <router-link tag="div" to="/analyse">
+      <i class="p-icon i-licai"></i>
       <span class="link">分析</span>
     </router-link>
     <router-link tag="div" to="/me">
+      <i class="p-icon i-wode"></i>
       <span class="link">我的</span>
     </router-link>
   </div>
@@ -22,9 +26,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "common/css/variable.scss";
+
 .p-footer {
-  height: 44px;
+  width: 100%;
+  height: 50px;
   position: fixed;
   bottom: 0px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    .p-icon {
+      font-size: 20px;
+    }
+    &.router-link-active {
+      .p-icon {
+        color: $color-primary;
+      }
+    }
+    span {
+      font-size: 14px;
+    }
+  }
 }
 </style>
--- src/components/record/record.vue
+++ src/components/record/record.vue
@@ -1,14 +1,34 @@
 <template>
-    <div>
-        流水
+  <div>
+    流水
+    <div class="add-one-wrapper">
+      <mt-button class="add-one" type="primary" @click.native="addOne">记一笔</mt-button>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  },
+  methods: {
+    addOne() {
+      console.log('add one')
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.add-one-wrapper {
+  width: 100%;
+  position: absolute;
+  bottom: 60px;
+  display: flex;
+  justify-content: space-around;
+  .add-one {
+    width: 90%;
+  }
+}
 </style>
