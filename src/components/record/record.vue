df0a1755e923e614b09b4558a5c2ba26227ca638
sdshaoda <sdshaoda@126.com>
2018-08-09 00:26:32 +0800

    add overview

--- src/components/record/record.vue
+++ src/components/record/record.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-    流水
+    <div class="overview">
+      <mt-swipe :auto="0">
+        <mt-swipe-item>
+          <div class="main">
+            <span class="label">本月支出</span>
+            <span class="amount">217.28</span>
+          </div>
+          <div class="sub">
+            <div class="left">
+              <span class="label">本月收入</span>
+              <span class="amount">9847.00</span>
+            </div>
+            <div class="right">
+              <span class="label">本月收入</span>
+              <span class="amount">9847.00</span>
+            </div>
+          </div>
+        </mt-swipe-item>
+        <mt-swipe-item>
+          <div class="main">
+            <span class="label">净资产</span>
+            <span class="amount">646523.28</span>
+          </div>
+          <div class="sub">
+            <div class="left">
+              <span class="label">总资产</span>
+              <span class="amount">9847.00</span>
+            </div>
+            <div class="right">
+              <span class="label">总负债</span>
+              <span class="amount">9847.00</span>
+            </div>
+          </div>
+        </mt-swipe-item>
+      </mt-swipe>
+    </div>
+    <div class="record">
+
+    </div>
     <div class="add-one-wrapper">
       <mt-button class="add-one" type="primary" @click.native="addOne">记一笔</mt-button>
     </div>
@@ -21,6 +59,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.overview {
+  width: 100%;
+  height: 200px;
+  background: #333;
+  .mint-swipe-item {
+    padding: 0 20px;
+    .main {
+      height: 140px;
+      padding-top: 70px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      .amount {
+        font-size: 30px;
+      }
+    }
+    .sub {
+      height: 50px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .right {
+        padding-right: 20px;
+      }
+    }
+    .label {
+      color: #ccc;
+    }
+    .amount {
+      color: #fff;
+    }
+  }
+}
 .add-one-wrapper {
   width: 100%;
   position: absolute;
