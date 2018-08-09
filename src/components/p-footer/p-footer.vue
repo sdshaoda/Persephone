9ed3c2151cbad4571976ad6980ce8f96671f36a8
dshao <dshao@i9i8.com>
2018-08-09 10:10:17 +0800

    update icon

--- src/components/p-footer/p-footer.vue
+++ src/components/p-footer/p-footer.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="p-footer">
     <router-link tag="div" to="/record">
-      <i class="p-icon i-jizhang"></i>
+      <i class="p-icon i-record"></i>
       <span class="link">流水</span>
     </router-link>
     <router-link tag="div" to="/asset">
-      <i class="p-icon i-caifua"></i>
+      <i class="p-icon i-wallet"></i>
       <span class="link">资产</span>
     </router-link>
     <router-link tag="div" to="/analyse">
-      <i class="p-icon i-licai"></i>
+      <i class="p-icon i-analyse"></i>
       <span class="link">分析</span>
     </router-link>
     <router-link tag="div" to="/me">
-      <i class="p-icon i-wode"></i>
+      <i class="p-icon i-me"></i>
       <span class="link">我的</span>
     </router-link>
   </div>
@@ -47,9 +47,7 @@ export default {
       font-size: 20px;
     }
     &.router-link-active {
-      .p-icon {
-        color: $color-primary;
-      }
+      color: $color-primary;
     }
     span {
       font-size: 14px;
