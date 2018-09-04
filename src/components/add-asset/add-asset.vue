faf8fc91c811f5a22e1823d61b301b10b543ab54
sdshaoda <sdshaoda@126.com>
2018-09-04 21:18:27 +0800

    add some DOM and style

--- src/components/add-asset/add-asset.vue
+++ src/components/add-asset/add-asset.vue
@@ -1,21 +1,25 @@
 <template>
   <transition name="upglide">
-    <div class="add-asset" @touchmove.stop.prevent="touchmove">
+    <div class="add-asset" ref="addAsset">
+
       <div class="header">
-        <span @click.stop.prevent="close">X</span>
-        <span>添加资产</span>
+        <span class="close" @click.stop.prevent="close">X</span>
+        <h1 class="title">添加资产</h1>
       </div>
+
       <div class="tab">
-        <span>资金</span>
-        <span>投资</span>
-        <span>应收</span>
-        <span>应付</span>
+        <span class="tab-item active">资金</span>
+        <span class="tab-item">投资</span>
+        <span class="tab-item">应收</span>
+        <span class="tab-item">应付</span>
       </div>
-      <div class="container" @touchmove.stop="contouchmove">
+
+      <div class="container" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
         <ul>
           <li v-for="item in 100" :key="item">{{ item }}</li>
         </ul>
       </div>
+
     </div>
   </transition>
 </template>
@@ -34,17 +38,22 @@ export default {
     close() {
       this.$router.push('/asset')
     },
+    touchstart() {
+      console.log('touchstart')
+    },
     touchmove() {
       console.log('touchmove')
     },
-    contouchmove() {
-      console.log('contouchmove')
+    touchend() {
+      console.log('touchend')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "common/css/variable.scss";
+
 .add-asset {
   width: 100%;
   height: 100%;
@@ -53,7 +62,6 @@ export default {
   left: 0;
   background: white;
   z-index: 10;
-  overflow: hidden;
   &.upglide-enter-active,
   &.upglide-leave-active {
     transition: all 0.3s;
@@ -62,7 +70,46 @@ export default {
   &.upglide-leave-to {
     transform: translate3d(0, 100%, 0);
   }
+  .header {
+    height: 45px;
+    width: 100%;
+    position: relative;
+    line-height: 45px;
+    .close {
+      padding-left: 0.5em;
+      position: absolute;
+      top: 0;
+      left: 0;
+      font-size: 22px;
+    }
+    .title {
+      text-align: center;
+      font-size: 20px;
+    }
+  }
+  .tab {
+    width: 80%;
+    height: 25px;
+    margin: 5px auto;
+    display: flex;
+    border: 1px solid #3e465b;
+    border-radius: 3px;
+    text-align: center;
+    font-size: 16px;
+    line-height: 25px;
+    * + * {
+      border-left: 1px solid #3e465b;
+    }
+    .tab-item {
+      width: 25%;
+      &.active {
+        background: #3e465b;
+        color: white;
+      }
+    }
+  }
   .container {
+    height: 600px;
     overflow: auto;
   }
 }
