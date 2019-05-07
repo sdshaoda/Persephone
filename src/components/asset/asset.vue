4d368a8f92c7f05155c8a50d76d6076838e5034c
sdshaoda <sdshaoda@126.com>
2019-05-08 00:14:29 +0800

    use scroll component

--- src/components/asset/asset.vue
+++ src/components/asset/asset.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="asset">
-    <div class="overview">
-      <div class="mt-swipe-item">
-        <div class="main">
-          <span class="label">本月支出</span>
-          <span class="amount">217.28</span>
-        </div>
-        <div class="sub">
-          <div class="left">
-            <span class="label">本月收入</span>
-            <span class="amount">9847.00</span>
+    <scroll class="asset-content">
+      <div>
+        <div class="overview">
+          <div class="mt-swipe-item">
+            <div class="main">
+              <span class="label">本月支出</span>
+              <span class="amount">217.28</span>
+            </div>
+            <div class="sub">
+              <div class="left">
+                <span class="label">本月收入</span>
+                <span class="amount">9847.00</span>
+              </div>
+              <div class="right">
+                <span class="label">本月收入</span>
+                <span class="amount">9847.00</span>
+              </div>
+            </div>
           </div>
-          <div class="right">
-            <span class="label">本月收入</span>
-            <span class="amount">9847.00</span>
-          </div>
-        </div>
-      </div>
-      <!-- <mt-swipe :auto="0">
+          <!-- <mt-swipe :auto="0">
         <mt-swipe-item>
           <div class="main">
             <span class="label">本月支出</span>
@@ -50,71 +52,74 @@
             </div>
           </div>
         </mt-swipe-item>
-      </mt-swipe>-->
-    </div>
+          </mt-swipe>-->
+        </div>
 
-    <div class="asset-overview">
-      <div class="title">资产账户</div>
+        <div class="asset-overview">
+          <div class="title">资产账户</div>
 
-      <cell-swipe
-        v-for="item in 3"
-        :key="item"
-        class="asset-item"
-        icon="yiban"
-        left-main="支付宝"
-        right-main="5954.80"
-        @contentClick="contentClick(item)"
-        @extendClick="extendClick"
-        :right="[{
-        content: '编辑',
-        style: {background: '#8c8c8c', color: 'white', width: '50px'},
-        handle: (item, index) => {num.splice(index, 1)}
-      },{
-        content: '删除',
-        style: {background: '#f5222d', color: 'white', width: '50px'},
-        handle: (item, index) => {num.splice(index, 1)}
-      }]"
-      ></cell-swipe>
-    </div>
+          <cell-swipe
+            v-for="item in 3"
+            :key="item"
+            class="asset-item"
+            icon="yiban"
+            left-main="支付宝"
+            right-main="5954.80"
+            @contentClick="contentClick(item)"
+            @extendClick="extendClick"
+            :right="[{
+              content: '编辑',
+              style: {background: '#8c8c8c', color: 'white', width: '50px'},
+              handle: (item, index) => {num.splice(index, 1)}
+            },{
+              content: '删除',
+              style: {background: '#f5222d', color: 'white', width: '50px'},
+              handle: (item, index) => {num.splice(index, 1)}
+            }]"
+          ></cell-swipe>
+        </div>
 
-    <div class="record-overview">
-      <div class="title">
-        近3日新增账单
-        <span>0笔</span>
+        <div class="record-overview">
+          <div class="title">
+            近3日新增账单
+            <span>0笔</span>
+          </div>
+
+          <cell-swipe
+            v-for="item in 10"
+            :key="item"
+            icon="yiban"
+            left-main="早午晚餐"
+            left-sub="2018-8-30 00:06:49"
+            right-main="12.00"
+            :right-main-style="{color: 'green'}"
+            right-sub="招商银行信用卡"
+            @contentClick="contentClick(item)"
+            @extendClick="extendClick"
+            :right="[{
+              content: '删除',
+              style: {background: '#f5222d', color: 'white', width: '50px'},
+              handle: (item, index) => {num.splice(index, 1)}
+            }]"
+          ></cell-swipe>
+        </div>
       </div>
 
-      <cell-swipe
-        v-for="item in 10"
-        :key="item"
-        icon="yiban"
-        left-main="早午晚餐"
-        left-sub="2018-8-30 00:06:49"
-        right-main="12.00"
-        :right-main-style="{color: 'green'}"
-        right-sub="招商银行信用卡"
-        @contentClick="contentClick(item)"
-        @extendClick="extendClick"
-        :right="[{
-        content: '删除',
-        style: {background: '#f5222d', color: 'white', width: '50px'},
-        handle: (item, index) => {num.splice(index, 1)}
-      }]"
-      ></cell-swipe>
-    </div>
-
-    <div class="add-wrapper">
-      <button class="add-record" @click.stop.prevent="addRecord">
-        <i class="p-icon i-add-record"></i>记一笔
-      </button>
-      |
-      <button class="add-asset" @click.stop.prevent="addAsset">添加资产</button>
-    </div>
+      <div class="add-wrapper">
+        <button class="add-record" @click.stop.prevent="addRecord">
+          <i class="p-icon i-add-record"></i>记一笔
+        </button>
+        |
+        <button class="add-asset" @click.stop.prevent="addAsset">添加资产</button>
+      </div>
+    </scroll>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import CellSwipe from 'base/cell-swipe/cell-swipe'
+import Scroll from 'base/scroll/scroll'
 
 export default {
   data() {
@@ -139,7 +144,8 @@ export default {
     }
   },
   components: {
-    CellSwipe
+    CellSwipe,
+    Scroll,
   }
 }
 </script>
@@ -148,95 +154,101 @@ export default {
 @import "common/css/variable.scss";
 
 .asset {
-  height: 100%;
-  padding-bottom: 100px;
-  .overview {
-    width: 100%;
-    height: 200px;
-    background: #333;
-    .mint-swipe-item {
-      padding: 0 20px;
-      .main {
-        height: 140px;
-        padding-top: 70px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 100px;
+  .asset-content {
+    height: 100%;
+    overflow: hidden;
+    .overview {
+      width: 100%;
+      height: 200px;
+      background: #333;
+      .mint-swipe-item {
+        padding: 0 20px;
+        .main {
+          height: 140px;
+          padding-top: 70px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          .amount {
+            font-size: 30px;
+          }
+        }
+        .sub {
+          height: 50px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          .right {
+            padding-right: 20px;
+          }
+        }
+        .label {
+          color: #ccc;
+        }
         .amount {
-          font-size: 30px;
+          color: #fff;
         }
       }
-      .sub {
-        height: 50px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .right {
-          padding-right: 20px;
-        }
-      }
-      .label {
-        color: #ccc;
-      }
-      .amount {
-        color: #fff;
+    }
+
+    .asset-overview,
+    .record-overview {
+      margin: 30px 0;
+    }
+
+    .asset-item {
+      height: 55px;
+      margin: 5px;
+      width: calc(100% - 10px);
+      box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
+      border-radius: 3px;
+      line-height: 55px;
+    }
+
+    .title {
+      padding-left: 1em;
+      line-height: 30px;
+      span {
+        color: $color-grey-7;
+        padding-left: 0.3em;
       }
     }
-  }
 
-  .asset-overview,
-  .record-overview {
-    margin: 30px 0;
-  }
-
-  .asset-item {
-    height: 55px;
-    margin: 5px;
-    width: calc(100% - 10px);
-    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
-    border-radius: 3px;
-    line-height: 55px;
-  }
-
-  .title {
-    padding-left: 1em;
-    line-height: 30px;
-    span {
-      color: $color-grey-7;
-      padding-left: 0.3em;
-    }
-  }
-
-  .add-wrapper {
-    width: calc(100% - 20px);
-    margin: 10px;
-    position: fixed;
-    bottom: 50px;
-    display: flex;
-    justify-content: space-around;
-    background: $color-red-6;
-    color: white;
-    border-radius: 3px;
-    line-height: 41px;
-    button {
-      border: none;
-      background: none;
+    .add-wrapper {
+      width: calc(100% - 20px);
+      margin: 10px;
+      position: fixed;
+      bottom: 50px;
+      display: flex;
+      justify-content: space-around;
+      background: $color-red-6;
       color: white;
-      padding: 10px 0;
-    }
-    button:active {
-      background: $color-red-7;
-    }
-    .add-record {
-      width: 70%;
-      font-size: 16px;
-      i {
-        margin-right: 0.3em;
+      border-radius: 3px;
+      line-height: 41px;
+      button {
+        border: none;
+        background: none;
+        color: white;
+        padding: 10px 0;
       }
-    }
-    .add-asset {
-      width: 30%;
-      font-size: 14px;
+      button:active {
+        background: $color-red-7;
+      }
+      .add-record {
+        width: 70%;
+        font-size: 16px;
+        i {
+          margin-right: 0.3em;
+        }
+      }
+      .add-asset {
+        width: 30%;
+        font-size: 14px;
+      }
     }
   }
 }
